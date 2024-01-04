a8fe47a3ac1dfa0fa7ae07a8c66cc8ab9c814497
sebbyapudo <sebbyapudo@gmail.com>
2024-01-05 01:03:41 +0300

    Refactored code | rendered the input component

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -1,34 +1,20 @@
+import Input from '../../components/Input';
 import './Colors.css'
 
 function Colors() {
-  return <div>
+  return (
+    <div>
 
-    <label className='sidebar-label-container color-title'>
-      <input type='radio' name='test2' />
-      <span className='checkmark'></span> All
-    </label>
-    <label className='sidebar-label-container'>
-      <input type='radio' name='test2' />
-      <span className='checkmark'></span> Black 
-    </label>
-    <label className='sidebar-label-container'>
-      <input type='radio' name='test2' />
-      <span className='checkmark'></span>  Blue
-    </label>
-    <label className='sidebar-label-container'>
-      <input type='radio' name='test2' />
-      <span className='checkmark'></span> Red
-    </label>
-    <label className='sidebar-label-container'>
-      <input type='radio' name='test2' />
-      <span className='checkmark'></span> Green
-    </label>
-    <label className='sidebar-label-container'>
-      <input type='radio' name='test2' />
-      <span className='checkmark'></span> White
-    </label>
+      <h2 className='sidebar-title color-title'> Colors</h2>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
 
-  </div>
+    </div>
+  );
+  
 }
 
 export default Colors;
--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -1,30 +1,20 @@
+import Input from '../../components/Input';
 import './Price.css'
 
 function Price() {
-  return <div className='ml'>
-    <h2 className='sidebar-title price-title'>Price</h2>
+  return (
+    <div className='ml'>
+      <h2 className='sidebar-title price-title'>Price</h2>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
 
-      <label className='sidebar-label-container'>
-        <input type='radio' name='test2' />
-        <span className='checkmark'></span> All
-      </label>
-      <label className='sidebar-label-container'>
-        <input type='radio' name='test2' />
-        <span className='checkmark'></span> $0 - $50
-      </label>
-      <label className='sidebar-label-container'>
-        <input type='radio' name='test2' />
-        <span className='checkmark'></span>  $50 - $100
-      </label>
-      <label className='sidebar-label-container'>
-        <input type='radio' name='test2' />
-        <span className='checkmark'></span> $100 - $150
-      </label>
-      <label className='sidebar-label-container'>
-        <input type='radio' name='test2' />
-        <span className='checkmark'></span> Over $150
-      </label>
-  </div>
+    </div>
+  );
+  
+  
 }
 
 export default Price;
